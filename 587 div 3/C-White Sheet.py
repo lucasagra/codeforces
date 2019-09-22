68af132b84a9c3cc599770a42f3e3cfea4e997a1
Lucas <lucasomena@hotmail.com>
2019-09-22 11:38:26 -0300

    correction

--- 587 div 3/C-White Sheet.py
+++ 587 div 3/C-White Sheet.py
@@ -3,60 +3,74 @@ x3, y3, x4, y4 = map(int, input().split())
 x5, y5, x6, y6 = map(int, input().split())
 
 def checkHeight():
+    # if ret2 is inside ret1
     if x3 > x1 and x4 < x2:
-        #terceiro ret tem que ser maior que o branco inteiro
-        if x5 <= x1 and x6 >=x2:
+        #if ret3 > ret1
+        if x5 <= x1 and x6 >= x2:
             return True
         else:
             return False
-    elif (x3 > x1 and x4 > x2) or (x4 < x2 and x3 < x1):
-        #terceiro ret tem que ser maior que a sobra
+
+    #if ret2 is intersecting part of ret1
+    elif (x3 > x1 and x3 < x2 and x4 >= x2) or (x4 < x2 and x4 > x1 and x3 <= x1):
+        #upper part
         if x3 > x1:
             if x5 <= x1 and x6 >= x3:
+
                 return True
             else:
                 return False
+        #bottom part
         else:
             if x5 <= x4 and x6 >= x2:
                 return True
             else:
                 return False
 
+    # if ret2 is not intersecting PART of ret1
     else:
-        #se o seg reto cobrir tudo ok
+        #if ret2 cover all ret1
         if x3 <= x1 and x4 >= x2:
             return True
         else:
+            #if ret3 cover all ret1
             if x5 <= x1 and x6 >= x2:
                 return True
             else:
+
                 return False
 
 def checkHeight2():
+    # if ret2 is inside ret1
     if y3 > y1 and y4 < y2:
-        #terceiro ret tem que ser maior que o branco inteiro
+        #if ret3 > ret1
         if y5 <= y1 and y6 >= y2:
             return True
         else:
             return False
-    elif (y3 > y1 and y4 > y2) or (y4 < y2 and y3 < y1):
-        #terceiro ret tem que ser maior que a sobra
+
+    #if ret2 is intersecting part of ret1
+    elif (y3 > y1 and y3 < y2 and y4 >= y2) or (y4 < y2 and y4 > y1 and y3 <= y1):
+        #upper part
         if y3 > y1:
             if y5 <= y1 and y6 >= y3:
                 return True
             else:
                 return False
+        #bottom part
         else:
             if y5 <= y4 and y6 >= y2:
                 return True
             else:
                 return False
 
+    # if ret2 is not intersecting PART of ret1
     else:
-        #se o seg reto cobrir tudo ok
+        #if ret2 cover all ret1
         if y3 <= y1 and y4 >= y2:
             return True
         else:
+            #if ret3 cover all ret1
             if y5 <= y1 and y6 >= y2:
                 return True
             else:
@@ -74,8 +88,9 @@ def checkSide():
             return True
         else:
             return False
+
+    # se cada um cobre uma parte
     else:
-    #se cada um cobre uma parte
         if (y3 <= y1 and y4 >= y2) and (y5 <= y1 and y6 >= y2):
             return True
         else:
@@ -101,7 +116,7 @@ def checkSide2():
             return False
 
 
-if (not checkHeight()) or (not checkHeight2()):
+if (checkHeight() == False) or (checkHeight2() == False):
     print("YES")
 else:
     if checkSide() or checkSide2():
