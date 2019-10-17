979817831daef4caa559fe802e11dece7a4daabd
Lucas <lucasomena@hotmail.com>
2019-10-17 17:54:53 -0300

    contest 593 div 2

--- 593 div 2/C-Labs.py
+++ 593 div 2/C-Labs.py
@@ -1,17 +1,12 @@
 n = int(input())
-s = ''
 
 for i in range(1, n+1):
     cur = i
-    s += str(i) + ' '
+    s = str(i) + ' '
     for k in range(n - 1):
         if k % 2 == 0:
             cur += (1 + 2 * (n - i))
         else:
             cur += (1 + 2 * (i - 1))
-
         s += str(cur) + ' '
-
-    s += '\n'
-
-print(s, end='')
+    print(s)
