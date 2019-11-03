0a66ddb2f6bea4fbe392bc50aa34d60bb32be1c9
Lucas <lucasomena@hotmail.com>
2019-11-03 08:44:14 -0300

    contest 597 div 2

--- 597 div  2/A.cpp
+++ 597 div  2/A.cpp
@@ -9,29 +9,6 @@ int gcd(int a, int b)
     return gcd(b, a % b);
 }
 
-void test (int a, int b){
-    int aux[100] = {0};
-    aux[0] = 1;
-
-    int i = 0;
-    cout << i << " TRUE --" << endl;
-    i = 1;
-    while(i < 100) {
-        if(i >= a && aux[i-a] == 1) {
-            aux[i] = 1;
-        } else if (i >= b && aux[i - b] == 1) {
-            aux[i] = 1;
-        }
-        
-        if(aux[i]) {
-            cout << i << " TRUE --" << endl;
-        } else {
-            cout << i << " false" << endl;
-        }
-
-        i++;
-    }
-}
 int main () {
 
     int t; cin >> t;
@@ -39,30 +16,8 @@ int main () {
     while(t--) {
         int a, b; cin >> a >> b;
         
-
-        // test(a,b);
-        if(a == 1 || b == 1) {
-            cout << "finite" << endl;
-            continue;
-        } else if (a == b) {
-            cout << "infinite" << endl;
-            continue;
-        }
-
-        if(b > a) {
-            int aux;
-            aux = a;
-            a = b;
-            b = aux;
-        }
-
-        int divisor = gcd(a,b);
-        if(divisor == 1) {
-            cout << "finite" << endl;
-        } else {
-            cout << "infinite" << endl;
-        }
-
+        if (gcd(a, b) == 1) cout << "finite" << endl;
+        else cout << "infinite" << endl;
     }
 
     return 0;
